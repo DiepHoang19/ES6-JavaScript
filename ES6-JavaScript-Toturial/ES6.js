1f402f9ccb97a42c724d25ded89e4373bd41ef47
DiepHoang19 <93962691+DiepHoang19@users.noreply.github.com>
2022-08-16 09:57:01 +0700

    update

---
+++ ES6-JavaScript-Toturial/ES6.js
@@ -0,0 +1,19 @@
+// ES6 let : const
+// block Scope
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+
+
+// ES5
+
+
+// var firstName = "ReactJs"
+
+// function getName(){
+//     var lastName = "JavaScript";
+
+//     return firstName + " " + lastName
+// }
+// console.log(getName())
